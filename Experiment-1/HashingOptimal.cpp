84371738872fa39b161d53c6037d09695460a8cf
fhznnxin-jpg <fhznnxin@gmail.com>
2026-07-22 14:07:28 +0530

    Rename function to Duplicate for clarity

--- Experiment-1/HashingOptimal.cpp
+++ Experiment-1/HashingOptimal.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
+
 bool Duplicate(vector<int>& nums, int k)
 {
     unordered_map<int, int> mp;
+
     for (int i = 0; i < nums.size(); i++)
     {
         if (mp.count(nums[i]))
@@ -12,24 +14,33 @@ bool Duplicate(vector<int>& nums, int k)
             if (i - mp[nums[i]] <= k)
                 return true;
         }
+
         mp[nums[i]] = i;
     }
+
     return false;
 }
+
 int main()
 {
     int n, k;
+
     cout << "Enter size: ";
     cin >> n;
+
     vector<int> nums(n);
+
     cout << "Enter elements: ";
     for (int i = 0; i < n; i++)
         cin >> nums[i];
+
     cout << "Enter k: ";
     cin >> k;
-    if (containsNearbyDuplicate(nums, k))
+
+    if (Duplicate(nums, k))
         cout << "true";
     else
         cout << "false";
+
     return 0;
 }
